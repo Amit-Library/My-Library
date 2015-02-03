5eab79e4543a88626161ed8860f1f01b5fa96647
Amit Kumar <amitk7075@gmail.com>
2015-02-03 18:48:49 +0530

    Paperclip used

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,7 @@ class Book < ActiveRecord::Base
 	def self.search(query)
   	where("book_name like ?", "%#{query}%") 
 	end
+	has_attached_file :photo
+	do_not_validate_attachment_file_type :photo
 
 end
